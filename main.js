9edd2681fd414792dcbe95ca01fbe61089b92e01
Anurag Joshi <anuragj990@gmail.com>
2025-01-10 20:47:41 +0530

    1st try

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   // TODO: Implement this function
+
+  if(req.method==="GET")
+  {
+    res.end(JSON.stringify({
+      "method": "GET",
+      "url": req.url
+    }))
+  }
+  else if(req.method==="POST")
+  {
+    res.end(JSON.stringify({
+      "method": "POST",
+      "url": req.url
+    }))
+  }
 });
 
 server.listen(3000, () => {
